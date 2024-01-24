a698bf0fbc321a901e839a059dca66facb47ade4
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-01-24 16:16:22 +0100

    basi

---
+++ src/main/java/domenico/DesignPatters/DesignPattersApplication.java
@@ -0,0 +1,13 @@
+package domenico.DesignPatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPattersApplication.class, args);
+	}
+
+}
